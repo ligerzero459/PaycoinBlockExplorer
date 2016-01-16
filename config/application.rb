347d7aa1bcf7f45ef81585ecc51b0e85f41fe314
Ryan Mottley <ligerzero459@gmail.com>
2016-01-16 00:48:10 -0600

    new attempt at pulling style.css into asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups)
 
 module PaycoinBlockExplorer
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("vendor","assets", "fonts")
+    config.assets.paths << Rails.root.join("vendor","assets", "fonts", "images")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
