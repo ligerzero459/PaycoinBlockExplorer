ccac01a3ed5212ca07def20a25606c93acc9e7ed
Ryan Mottley <ligerzero459@gmail.com>
2016-02-11 14:22:37 -0600

    search: fix search bug not finding blocks

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,7 +4,7 @@ class SearchesController < ApplicationController
   # GET /searches
   # GET /searches.json
   def index
-    if (params[:search].strip.length <= 1)
+    if (params[:search].strip.length <= 7)
       block = Block.find(:height=>params[:search].strip)
     end
     blocks = Block.where(Sequel.like(:blockHash, params[:search].strip + "%")).limit(4)
